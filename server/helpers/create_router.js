ea105c7c5e873f4332aa631d0f9e3b973c18a216
Daniel Stewart <mgdog1@googlemail.com>
2018-07-07 09:30:24 +0100

    added fix to stop back to back locations

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -6,18 +6,20 @@ const createRouter = function (collection) {
   const router = express.Router();
 
   router.get('/random-landmark', (req, res) => {
-    collection
-    .find()
-    .toArray()
-    .then((docs) => {
-      return res.json(docs)
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500);
-      res.json({ status: 500, error: err });
+    collection.aggregate([
+      {$match:{"_id":{$ne:router.lastId}}},
+      {$sample: {size: 1}}])
+      .toArray()
+      .then((docs) => {
+        router.lastId = docs[0]._id;
+        return res.json(docs[0])
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
     });
-  });
 
   return router;
 
