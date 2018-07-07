ba42bef120dca7001ccde2df828ab1ed2318d54e
Daniel Stewart <mgdog1@googlemail.com>
2018-07-07 09:44:38 +0100

    added cycling to random picks

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -4,14 +4,20 @@ const ObjectID = require('mongodb').ObjectID;
 const createRouter = function (collection) {
 
   const router = express.Router();
+  router.idsSent = [];
+  collection.countDocuments().then(count => router.numDocuments =  count);
+
 
   router.get('/random-landmark', (req, res) => {
     collection.aggregate([
-      {$match:{"_id":{$ne:router.lastId}}},
+      {$match:{"_id":{$nin:router.idsSent}}},
       {$sample: {size: 1}}])
       .toArray()
       .then((docs) => {
-        router.lastId = docs[0]._id;
+        if(router.idsSent.length === router.numDocuments - 1){
+          router.idsSent = [];
+        }
+        router.idsSent.push(docs[0]._id);
         return res.json(docs[0])
       })
       .catch((err) => {
