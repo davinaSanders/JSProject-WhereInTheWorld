837ab595e15d3551280f78f6377aaec8e3d96c67
Daniel Stewart <mgdog1@googlemail.com>
2018-07-09 09:26:32 +0100

    ignore this

--- client/src/views/landmark_view.js
+++ client/src/views/landmark_view.js
@@ -4,18 +4,39 @@ const LandmarkView = function (container) {
   this.container = container;
 };
 
-
 LandmarkView.prototype.initialise = function () {
   PubSub.subscribe('Landmark:landmark-loaded', (event) => {
     this.display(event.detail);
   });
+
+  const fenway = {lat: 42.345573, lng: -71.098326};
+  this.map = new google.maps.Map(this.container, {
+    center: fenway,
+    zoom: 14
+  });
+  this.panorama = new google.maps.StreetViewPanorama(
+      document.getElementById('pano'), {
+        position: fenway,
+        pov: {
+          heading: 34,
+          pitch: 10
+        }
+      });
+  this.map.setStreetView(this.panorama);
 };
 
 LandmarkView.prototype.display = function (landmark) {
-  this.container.innerHTML = " ";
-  const image = document.createElement('img')
-  image.src = `./images/${landmark.image}`;
-  this.container.appendChild(image);
+  console.log(landmark);
+  const landmarkCoords = {lat: landmark.lat, lng:landmark.long};
+  this.panorama = new google.maps.StreetViewPanorama(
+      document.getElementById('pano'), {
+        position: landmarkCoords,
+        pov: {
+          heading: 34,
+          pitch: 10
+        }
+      });
+  this.map.setStreetView(this.panorama);
 };
 
 module.exports = LandmarkView;
